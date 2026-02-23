6cfc1484a44bca95d832aba0c460289cdcf0a2f6
Fahmi Rosadi <117837233+FahmiRosadi@users.noreply.github.com>
2026-02-23 16:17:18 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Jenis Berita Terkini / Terpopuler
+function showContent(contentName) {
+  var contents = document.querySelectorAll(".berita-terkini");
+  for (var i = 0; i < contents.length; i++) {
+    contents[i].style.display = "none";
+  }
+  document.getElementById(contentName + "-content").style.display = "flex";
+}
+
+// Live carousel
+document.addEventListener("DOMContentLoaded", function () {
+  var myCarousel = document.querySelector("#carouselExampleDark");
+  var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 2000,
+    wrap: true,
+  });
+});
+
+// Modal Ulasan
+function kirimUlasan() {
+  var rating = document.querySelector('input[name="rating"]:checked');
+  var ulasan = document.getElementById("ulasan").value;
+
+  if (!rating) {
+    alert("Silakan pilih rating.");
+    return;
+  }
+
+  var nilaiRating = rating.value;
+
+  console.log("Rating: " + nilaiRating);
+  console.log("Ulasan: " + ulasan);
+
+  // Tutup modal
+  $("#ulasanModal").modal("hide");
+}
+
+// Memastikan hanya satu bintang yang dipilih
+const ratings = document.querySelectorAll(".rating input");
+ratings.forEach((radio) => {
+  radio.addEventListener("click", (event) => {
+    ratings.forEach((r) => {
+      r.checked = false;
+    });
+    event.target.checked = true;
+  });
+});
